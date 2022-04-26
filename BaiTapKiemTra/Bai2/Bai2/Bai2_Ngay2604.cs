944a4e9c17a4671d0f64388225b75bdac9a41348
TriHung1704 <94167202+TriHung1704@users.noreply.github.com>
2022-04-26 12:07:43 +0700

    Add files via upload

---
+++ BaiTapKiemTra/Bai2/Bai2/Bai2_Ngay2604.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    abstract class Animal
+    {
+        public abstract void eat();
+        public abstract void makeSound();
+    }
+    class Bird : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating ");
+        }
+
+        public void fly()
+        {
+            Console.WriteLine("Flying");
+        }
+
+        public override void makeSound()
+        {
+            Console.WriteLine("Sounding");
+
+        }
+    }
+    class Cat : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating Cat");
+
+        }
+        public void run()
+        {
+            Console.WriteLine("Running");
+        }
+
+        public override void makeSound()
+        {
+            Console.WriteLine("Sounding Cat");
+        }
+    }
+    class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat();
+            cat.eat();
+            cat.makeSound();
+            Animal bird = new Bird();
+            bird.eat();
+            bird.makeSound();
+            Console.ReadKey();
+        }
+    }
+}
